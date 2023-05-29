13f5f45a82d8a73edae2040109446f6105536789
marceop <marta.op18@gmail.com>
2023-05-29 20:31:42 +0200

    feat: (settinp up the function to create the board using a new div with its square class

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// BRING THE WHOLE DOCUMENT WITH THe   querySelector  METHOD
+// BRINGING THE WHOLE DOCUMENT WITH THe   querySelector  METHOD
 // The Document method querySelector() returns the first Element within the document that matches the specified selector, or group of selectors. If no matches are found, null is returned.
 
 
@@ -8,3 +8,14 @@ const howToPlay = document.querySelector('#howtoplay');
 // CREATING BOARD CELLS ARRAY
 
 const boardCells = ['', '', '', '', '', '', '', '', ''];
+
+// SETTING UP FUNCTION TO CREATE THE BOARD WITH FOR-EACH
+// then create a new const to build the board with a div and the 9 squares with a class
+
+const createBoard = () =>  {
+    boardCells.forEach((cell, index) => {
+        const cellElement = document.createElement('div')
+        cellElement.classList.add('square')
+    })
+
+}
