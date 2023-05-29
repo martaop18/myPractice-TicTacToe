613a532c44b9ebaf7ddb828c368a6412a1912830
marceop <marta.op18@gmail.com>
2023-05-29 20:01:19 +0200

    feat: (bringing the whole html to js with the queryselector method)

---
+++ main.js
@@ -0,0 +1,9 @@
+// BRING THE WHOLE DOCUMENT WITH THe   querySelector  METHOD
+// The Document method querySelector() returns the first Element within the document that matches the specified selector, or group of selectors. If no matches are found, null is returned.
+
+
+const gameBoard = document.querySelector('#gameboard');
+const howToPlay = document.querySelector('#howtoplay');
+
+// CREATING BOARD CELLS ARRAY
+
