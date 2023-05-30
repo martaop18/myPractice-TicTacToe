56bacd6dea9e543542b0deefa941288a03f059c2
marceop <marta.op18@gmail.com>
2023-05-30 19:42:11 +0200

    feat: (defining function if circles win)

--- main.js
+++ main.js
@@ -15,37 +15,47 @@ playDisplay.textContent = 'circle goes first'
 // SETTING UP FUNCTION TO CREATE THE BOARD WITH FOR-EACH
 // then create a new const to build the board with a div and the 9 squares with a class
 
-const createBoard = () =>  {
+const createBoard = () => {
     boardCells.forEach((_cell, index) => {
         const cellElement = document.createElement('div');
         cellElement.classList.add('square');
         cellElement.id = index
-        cellElement.addEventListener('click', goClick);
+        cellElement.addEventListener('click', addGo);
         gameBoard.append(cellElement);
     });
 
 }
+
 createBoard();
 
 // CREATING FUNCTION TO PRINT CIRCLES AND CROSSES GOCLICK
-function goClick (e) {
+function addGo(e) {
     // console.log(e.target)
 const goDisplay = document.createElement('div');
 goDisplay.classList.add(go);
 e.target.append(goDisplay);
 go = go === 'circle' ? 'cross' : 'circle'
-playDisplay.textContent = 'it is now ' + go + "'s go"
-e.target.removeEventlistener('click', goClick)
+playDisplay.textContent = "It is now " + go + "'s turn."
+e.target.removeEventListener("click", addGo)
 checkScore()
 
 function checkScore() {
+    const allSquares = document.querySelectorAll(".square");
+    // console.log(allSquares);
+
     // SETING WINNING COMBOS ARRAYS
     const winningCombos = [ 
         [0,1,2],[3,4,5],[6,7,8],
         [0,3,6],[1,4,7],[2,5,8],
         [0,4,8],[2,4,6]   
-    
     ]
+    winningCombos.forEach(array => {
+        const circleWins = array.every(cell => allSquares[cell].firstChild?.classList.contains('circle'))
+        if (circleWins) {
+            playDisplay.textContent = "You're the winner!"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+        }
+    })
 }
 }
 
